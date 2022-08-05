9aef8b01ba1e034967d1febccf752f7eb25161a9
SpatioRex <109388897+SpatioRex@users.noreply.github.com>
2022-08-05 16:22:08 -0700

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,28 @@
+x = 5
+name = 'mack'
+y = 3.2
+true = True
+## dir(name)
+### print(name.upper())
+### help()
+### for i in range(3):
+###    print(name)
+## for i in range (3):
+##    print(i)
+## help(range)
+## for i in range (1,5):
+##    print (i)
+
+## for i in range (2, 10, 2):
+##    print(i)
+
+## l_5 = list(range(5))
+## for num in l_5:
+##    print (num)
+##    print()
+##    print(num *2)
+##    print()
+print (name[0:3])
+print (name[:7])
+print (name[0:4:3])
+print (name[0:4])
